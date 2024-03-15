b0d6049bfcabb91e4596e86461473fe517f8725c
Johnson <oyebodej2@gmail.com>
2024-03-15 13:25:17 +0100

    creating and sending mail with attachment

--- src/main/java/com/example/sendmail/EmailSenderService.java
+++ src/main/java/com/example/sendmail/EmailSenderService.java
@@ -1,9 +1,15 @@
 package com.example.sendmail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailSenderService {
@@ -22,6 +28,26 @@ public class EmailSenderService {
         message.setSubject(subject);
 
         javaMailSender.send(message);
+        System.out.println("Mail Send...");
+    }
+
+    public void sendEmailWithAttachment(String toEmail,
+                                        String body,
+                                        String subject,
+                                        String attachment) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom("oyebodej2@gmail.com");
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystemResource =
+                new FileSystemResource(new File(attachment));
+        mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+
+        javaMailSender.send(mimeMessage);
         System.out.println("Mail Send...");
     }
 }
